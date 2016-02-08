8a16d327540ded220d1dee6d3efa65672409cdda
Klaus Alexander Seistrup <klaus@seistrup.dk>
2016-02-08 20:35:50 +0100

    Initial commit

---
+++ templates/template.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# -*- mode: sh; coding: utf-8 -*-
+# Auto-generated on ${UTCFULLDOW}, ${UTCISODATE} at ${UTCHHMM24} UTC.
+# This shell script is a part of the Repickle project:
+#  · https://github.com/kseistrup/repickle
+# q.v.
+exec repickle --input ${FROM} --output ${TO} "${@}"
+# eof
